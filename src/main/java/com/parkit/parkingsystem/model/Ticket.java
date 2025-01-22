5e06b98e6adfaf4482dcfaba3532002928083b6d
lagourou <agourou84@gmail.com>
2025-01-23 10:54:55 +1100

    supprimer la méthode getHours car elle estobselète avec outHours et intHours car cela ne prend pas en compte les jours et entraîne des valeurs négatives lorsque le temps de sortie est inférieur au temps d'entrée, ecriture du test pour avoir le parking gratuit si la durée est infèrieur à 30 minutes, convertir la durée en minutes et en heures, déclaration de la variable rate pour stockerle tarif horaire en fonction du type de véhicule, ajouter comme parametre discount à la méthode calculateFare le mettre à true si la remise de 5%  est appliqué au tarif, création de ce test.

--- src/main/java/com/parkit/parkingsystem/model/Ticket.java
+++ src/main/java/com/parkit/parkingsystem/model/Ticket.java
@@ -1,6 +1,5 @@
 package com.parkit.parkingsystem.model;
 
-import java.util.Calendar;
 import java.util.Date;
 
 public class Ticket {
@@ -10,6 +9,7 @@ public class Ticket {
     private double price;
     private Date inTime;
     private Date outTime;
+    public int setOutTime;
 
     public int getId() {
         return id;
@@ -57,5 +57,10 @@ public class Ticket {
 
     public void setOutTime(Date outTime) {
         this.outTime = outTime;
+    }
+
+    public void setDuration(int i) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setDuration'");
     }
 }
--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -5,27 +5,53 @@ import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
-        if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
-            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
+    /**
+     * @param ticket
+     * @param discount
+     * 
+     */
+    public void calculateFare(Ticket ticket, boolean discount) {
+
+        if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
+            throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        long inTime = ticket.getInTime().getTime();
+        long outTime = ticket.getOutTime().getTime();
 
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+        // TODO: Some tests are failing here. Need to check if this logic is correct
 
-        switch (ticket.getParkingSpot().getParkingType()){
+        long duration = outTime - inTime;
+        double minutes = duration / (1000.0 * 60);
+        double hours = minutes / 60.0;
+
+        if (minutes <= 30) {
+            ticket.setPrice(0);
+            return;
+        }
+        double rate;
+
+        switch (ticket.getParkingSpot().getParkingType()) {
             case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                rate = Fare.CAR_RATE_PER_HOUR;
+                ticket.setPrice(hours * rate);
                 break;
             }
             case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                rate = Fare.BIKE_RATE_PER_HOUR;
+                ticket.setPrice(hours * rate);
                 break;
             }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
+            default:
+                throw new IllegalArgumentException("Unkown Parking Type");
+        }
+        if (discount) {
+            ticket.setPrice(ticket.getPrice() * 0.95);
         }
     }
+
+    public void calculateFare(Ticket ticket) {
+        calculateFare(ticket, false);
+    }
+
 }
