4c4ea059d5b991f8cc00aef2020010820ba88726
lagourou <agourou84@gmail.com>
2025-02-24 22:16:49 +1100

    Modification des testsTicketDAOTest et ParkingSpotDAOTest et correction des commentaires dans ces deux tests

--- src/test/java/com/parkit/parkingsystem/ParkingSpotDAOTest.java
+++ src/test/java/com/parkit/parkingsystem/ParkingSpotDAOTest.java
@@ -43,15 +43,18 @@ class ParkingSpotDAOTest {
     private ParkingSpotDAO parkingSpotDAO;
 
     @BeforeEach
-    void setUp() throws SQLException, ClassNotFoundException {
+    void setUp() throws Exception, SQLException {
+        // Initialise les mocks
         MockitoAnnotations.openMocks(this);
 
+        // Simule la connection à la base de données et la préparation des requêtes SQL
         when(dataBaseConfig.getConnection()).thenReturn(connection);
         when(connection.prepareStatement(DBConstants.GET_NEXT_PARKING_SPOT)).thenReturn(preparedStatement);
         when(connection.prepareStatement(DBConstants.UPDATE_PARKING_SPOT)).thenReturn(preparedStatement);
         when(connection.prepareStatement(DBConstants.GET_PARKING_SPOT)).thenReturn(preparedStatement);
     }
 
+    // Test pour simuler la situation ou un emplacement est disponible
     @Test
     void testGetNextAvailableSlot_Success() throws SQLException {
         // Simuler la connexion à la base de données et la requête SQL
@@ -59,12 +62,12 @@ class ParkingSpotDAOTest {
 
         when(preparedStatement.executeQuery()).thenReturn(resultSet);
         when(resultSet.next()).thenReturn(true);
-        when(resultSet.getInt(1)).thenReturn(5); // ID du prochain parking disponible
+        when(resultSet.getInt(1)).thenReturn(5); // ID du prochain stationnement
 
-        // Appeler la méthode à tester
+        // Appel de la méthode à tester
         int nextAvailableSlot = parkingSpotDAO.getNextAvailableSlot(parkingType);
 
-        // Vérifier que la méthode renvoie correctement l'ID du parking disponible
+        // Vérifier que la méthode renvoie correctement l'ID du prochain stationnement
         assertEquals(5, nextAvailableSlot);
 
         // Vérifier que la requête a été préparée avec le bon type de stationnement
@@ -72,15 +75,16 @@ class ParkingSpotDAOTest {
         verify(preparedStatement).executeQuery();
     }
 
+    // Test pour simuler la situation où aucun emplacement n'est disponible
     @Test
     void testGetNextAvailableSlot_NoSlotAvailable() throws SQLException {
-        // Simuler la situation où aucun emplacement n'est disponible
+        // Simuler la connexion à la base de données et la requête SQL
         ParkingType parkingType = ParkingType.BIKE;
 
         when(preparedStatement.executeQuery()).thenReturn(resultSet);
         when(resultSet.next()).thenReturn(false); // Aucun résultat
 
-        // Appeler la méthode à tester
+        // Appel de la méthode à tester
         int nextAvailableSlot = parkingSpotDAO.getNextAvailableSlot(parkingType);
 
         // Vérifier que la méthode retourne -1 lorsque aucun emplacement n'est
@@ -88,6 +92,8 @@ class ParkingSpotDAOTest {
         assertEquals(-1, nextAvailableSlot);
     }
 
+    // Test pour simuler la situation ou un emplacement est disponible avec une
+    // erreur
     @Test
     void testGetNextAvailableSlot_ExceptionHandling() throws SQLException {
         // Simuler une exception lors de l'exécution de la requête SQL
@@ -95,7 +101,7 @@ class ParkingSpotDAOTest {
 
         when(preparedStatement.executeQuery()).thenThrow(new SQLException("Database error"));
 
-        // Appeler la méthode à tester
+        // Appel de la méthode à tester
         int nextAvailableSlot = parkingSpotDAO.getNextAvailableSlot(parkingType);
 
         // Vérifier que la méthode retourne -1 en cas d'erreur
@@ -104,16 +110,15 @@ class ParkingSpotDAOTest {
 
     @Test
     void testGetNextParkingSpot_Success() throws SQLException {
-        // Simuler un emplacement disponible
+        // Simulation d'un emplacement disponible
         ParkingType parkingType = ParkingType.CAR;
         when(preparedStatement.executeQuery()).thenReturn(resultSet);
         when(resultSet.next()).thenReturn(true);
-        when(resultSet.getInt(1)).thenReturn(5); // ID du prochain parking disponible
-
-        // Appeler la méthode à tester
+        when(resultSet.getInt(1)).thenReturn(5); // ID de la prochaine place de parking
+        // Appel de la méthode à tester
         ParkingSpot nextSpot = parkingSpotDAO.getNextParkingSpot(parkingType);
 
-        // Vérifier que le parking spot retourné est celui attendu
+        // Vérifier que la place de parking retourné est celui attendu
         assertNotNull(nextSpot);
         assertEquals(5, nextSpot.getId());
         assertTrue(nextSpot.isAvailable());
@@ -126,12 +131,12 @@ class ParkingSpotDAOTest {
         ParkingType parkingType = ParkingType.BIKE;
 
         when(preparedStatement.executeQuery()).thenReturn(resultSet);
-        when(resultSet.next()).thenReturn(false); // Aucun emplacement trouvé
+        when(resultSet.next()).thenReturn(false); // Aucune place de parking trouvée
 
-        // Appeler la méthode
+        // Appel de la méthode
         ParkingSpot nextSpot = parkingSpotDAO.getNextParkingSpot(parkingType);
 
-        // Vérifier que le résultat est nul lorsque aucun emplacement n'est trouvé
+        // Vérifier que le résultat est nul lorsque aucune place de parking n'est trouvé
         assertNull(nextSpot);
     }
 
@@ -144,7 +149,7 @@ class ParkingSpotDAOTest {
         when(connection.prepareStatement(DBConstants.UPDATE_PARKING_SPOT)).thenReturn(preparedStatement);
         when(preparedStatement.executeUpdate()).thenReturn(1); // 1 ligne mise à jour
 
-        // Appeler la méthode à tester
+        // Appel de la méthode à tester
         boolean result = parkingSpotDAO.updateParking(parkingSpot);
 
         // Vérifier que la méthode retourne true (mise à jour réussie)
@@ -165,12 +170,13 @@ class ParkingSpotDAOTest {
         when(connection.prepareStatement(DBConstants.UPDATE_PARKING_SPOT)).thenReturn(preparedStatement);
         when(preparedStatement.executeUpdate()).thenReturn(0); // Aucune ligne mise à jour
 
-        // Appeler la méthode à tester
+        // Appel de la méthode à tester
         boolean result = parkingSpotDAO.updateParking(parkingSpot);
 
         // Vérifier que la méthode retourne false (mise à jour échouée)
         assertFalse(result);
 
+        // Vérifier que la requête préparée est bien configurée avec les bons paramètres
         verify(preparedStatement).setBoolean(1, parkingSpot.isAvailable());
         verify(preparedStatement).setInt(2, parkingSpot.getId());
         verify(preparedStatement).executeUpdate();
@@ -185,7 +191,7 @@ class ParkingSpotDAOTest {
         when(connection.prepareStatement(DBConstants.UPDATE_PARKING_SPOT)).thenReturn(preparedStatement);
         when(preparedStatement.executeUpdate()).thenThrow(new SQLException("Database error"));
 
-        // Appeler la méthode à tester
+        // Appel de la méthode à tester
         boolean result = parkingSpotDAO.updateParking(parkingSpot);
 
         // Vérifier que la méthode retourne false en cas d'exception
@@ -193,7 +199,7 @@ class ParkingSpotDAOTest {
     }
 
     @Test
-    void testGetParkingSpot_Success() throws SQLException, ClassNotFoundException {
+    void testGetParkingSpot_Success() throws Exception, SQLException {
         // Créer un objet ParkingSpot simulé pour le test
         int parkingSpotId = 1;
         ParkingType parkingType = ParkingType.CAR;
@@ -207,14 +213,14 @@ class ParkingSpotDAOTest {
         when(resultSet.getString("TYPE")).thenReturn(parkingType.toString());
         when(resultSet.getBoolean("available")).thenReturn(isAvailable);
 
-        // Appeler la méthode à tester
+        // Appel de la méthode à tester
         ParkingSpot result = parkingSpotDAO.getParkingSpot(parkingSpotId);
 
         // Vérifier que la méthode retourne un objet ParkingSpot avec les bons attributs
-        assertNotNull(result, "La place de parking ne doit pas être nulle");
-        assertEquals(parkingSpotId, result.getId(), "L'ID de la place de parking doit correspondre");
-        assertEquals(parkingType, result.getParkingType(), "Le type de la place de parking doit correspondre");
-        assertEquals(isAvailable, result.isAvailable(), "La disponibilité de la place de parking doit correspondre");
+        assertNotNull(result);
+        assertEquals(parkingSpotId, result.getId());
+        assertEquals(parkingType, result.getParkingType());
+        assertEquals(isAvailable, result.isAvailable());
 
         // Vérifier que la requête préparée est configurée correctement
         verify(preparedStatement).setInt(1, parkingSpotId);
@@ -222,7 +228,7 @@ class ParkingSpotDAOTest {
     }
 
     @Test
-    void testGetParkingSpot_NoSpotFound() throws SQLException, ClassNotFoundException {
+    void testGetParkingSpot_NoSpotFound() throws Exception, SQLException {
         // Créer un parking spotId pour le test
         int parkingSpotId = 1;
 
@@ -231,7 +237,7 @@ class ParkingSpotDAOTest {
         when(preparedStatement.executeQuery()).thenReturn(resultSet);
         when(resultSet.next()).thenReturn(false); // Aucun enregistrement trouvé
 
-        // Appeler la méthode à tester
+        // Appel de la méthode à tester
         ParkingSpot result = parkingSpotDAO.getParkingSpot(parkingSpotId);
 
         // Vérifier que la méthode retourne null si aucun emplacement n'est trouvé
@@ -251,7 +257,7 @@ class ParkingSpotDAOTest {
         when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
         when(preparedStatement.executeQuery()).thenThrow(new SQLException("Database error"));
 
-        // Appeler la méthode à tester
+        // Appel de la méthode à tester
         ParkingSpot result = parkingSpotDAO.getParkingSpot(parkingSpotId);
 
         // Vérifier que la méthode retourne null en cas d'exception
--- src/test/java/com/parkit/parkingsystem/TicketDAOTest.java
+++ src/test/java/com/parkit/parkingsystem/TicketDAOTest.java
@@ -13,11 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import org.mockito.Mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -34,7 +30,6 @@ class TicketDAOTest {
 
     private TicketDAO ticketDAO;
     private static final String VEHICLE_REG_NUMBER = "ABCDEF";
-    private static final Logger logger = LogManager.getLogger(TicketDAOTest.class);
 
     @Mock
     private DataBaseConfig dataBaseConfig;
@@ -48,11 +43,14 @@ class TicketDAOTest {
     private Ticket ticket;
 
     @BeforeEach
-    void setUp() throws ClassNotFoundException, SQLException, IllegalArgumentException, NoSuchFieldException {
+    void setUp() throws Exception, SQLException {
+        // Initialise les mocks
         MockitoAnnotations.openMocks(this);
+        // Initialise l'objet ticketDAO
         ticketDAO = new TicketDAO();
         ticketDAO.setDataBaseConfig(dataBaseConfig);
 
+        // Simule la connection à la base de données et la préparation des requêtes SQL
         when(dataBaseConfig.getConnection()).thenReturn(connection);
         when(connection.prepareStatement(any(String.class))).thenReturn(preparedStatement);
         when(preparedStatement.executeUpdate()).thenReturn(1);
@@ -63,106 +61,132 @@ class TicketDAOTest {
     }
 
     @Test
-    void testSaveTicket_Success() throws ClassNotFoundException, SQLException {
+    void testSaveTicket_Success() throws Exception, SQLException {
+        // Créer un nouveau ticket avec des valeurs spécifiques
         ticket = new Ticket();
         ticket.setParkingSpot(new ParkingSpot(1, null, false));
         ticket.setVehicleRegNumber(VEHICLE_REG_NUMBER);
         ticket.setPrice(15.0);
         ticket.setInTime(new Timestamp(System.currentTimeMillis()));
 
+        // Simule l'exécution de la requete UPADTE pour qu'elle retourne 1: mise à jour
+        // résussi
         when(preparedStatement.executeUpdate()).thenReturn(1);
 
+        // Appel de la méthode saveTicket du ticketDAO
         boolean result = ticketDAO.saveTicket(ticket);
 
+        // Vérifie que le résultat est vrai
         assertTrue(result);
+        // Vérifie que les méthodes attendues on été appelées une fois
         verify(dataBaseConfig, times(1)).getConnection();
         verify(connection, times(1)).prepareStatement(any(String.class));
         verify(preparedStatement, times(1)).executeUpdate();
         verify(dataBaseConfig, times(1)).closePreparedStatement(preparedStatement);
         verify(dataBaseConfig, times(1)).closeConnection(connection);
-        logger.info("Test SaveTicket_Success Passed");
     }
 
     @Test
-    void testSaveTicket_Failure() throws ClassNotFoundException, SQLException {
+    void testSaveTicket_Failure() throws Exception, SQLException {
+        // Créer un nouveau ticket avec des valeurs spécifiques
         ticket = new Ticket();
         ticket.setParkingSpot(new ParkingSpot(1, null, false));
         ticket.setVehicleRegNumber(VEHICLE_REG_NUMBER);
         ticket.setPrice(15.0);
         ticket.setInTime(new Timestamp(System.currentTimeMillis()));
 
+        // Simule l'exécution de la requete UPADTE pour qu'elle lance une exception:
+        // mise à jour à échoué
         when(preparedStatement.executeUpdate()).thenThrow(new SQLException("Error saving ticket"));
 
+        // Appel de la méthode saveTicket du ticketDAO
         boolean result = ticketDAO.saveTicket(ticket);
 
+        // Vérifie que le résultat est faux
         assertFalse(result);
+
+        // Vérifie que les méthodes attendues on été appelées une fois
         verify(dataBaseConfig, times(1)).getConnection();
         verify(connection, times(1)).prepareStatement(DBConstants.SAVE_TICKET);
         verify(preparedStatement, times(1)).executeUpdate();
         verify(dataBaseConfig, times(1)).closePreparedStatement(preparedStatement);
         verify(dataBaseConfig, times(1)).closeConnection(connection);
-        logger.info("Test SaveTicket_Failure Passed");
     }
 
     @Test
     void testSaveTicket_Exception() throws Exception {
+        // Créer un nouveau ticket avec des valeurs spécifiques
         ticket = new Ticket();
         ticket.setParkingSpot(new ParkingSpot(1, null, false));
         ticket.setVehicleRegNumber(VEHICLE_REG_NUMBER);
         ticket.setPrice(15.0);
         ticket.setInTime(new Timestamp(System.currentTimeMillis()));
 
+        // Simule que l'exécution de la requête UPDATE lance une SQLException
         when(preparedStatement.executeUpdate()).thenThrow(new SQLException());
 
+        // Appel de la méthode saveTicket du ticketDAO
         boolean result = ticketDAO.saveTicket(ticket);
 
+        // Vérifie que le résultat est faux: sauvegarde échouée
         assertFalse(result);
+        // Vérifie que les méthodes attendues on été appelées une fois
         verify(dataBaseConfig, times(1)).getConnection();
         verify(connection, times(1)).prepareStatement(DBConstants.SAVE_TICKET);
         verify(preparedStatement, times(1)).executeUpdate();
     }
 
+    // Test de le méthode saveTicket lorsque le nombre de lignes est supèrieur à 0
     @Test
     void testSaveTicket_RowsAffectedGreaterThanZero() throws Exception {
+        // Créer un nouveau ticket avec des valeurs spécifiques
         ticket = new Ticket();
         ticket.setParkingSpot(new ParkingSpot(1, null, false));
         ticket.setVehicleRegNumber(VEHICLE_REG_NUMBER);
         ticket.setPrice(15.0);
         ticket.setInTime(new Timestamp(System.currentTimeMillis()));
 
+        // Simule que 1 ligne à été affectée par la requête
         when(preparedStatement.executeUpdate()).thenReturn(1);
 
+        // Appel de la méthode saveTicket
         boolean result = ticketDAO.saveTicket(ticket);
 
+        // Vérifie que la méthode renvoie true
         assertTrue(result);
         verify(dataBaseConfig, times(1)).getConnection();
         verify(connection, times(1)).prepareStatement(DBConstants.SAVE_TICKET);
         verify(preparedStatement, times(1)).executeUpdate();
     }
 
+    // Test de le méthode saveTicket lorsque le nombre de lignes est égale à 0
     @Test
     void testSaveTicket_RowsAffectedEqualsZero() throws Exception {
+        // Créer un nouveau ticket avec des valeurs spécifiques
         ticket = new Ticket();
         ticket.setParkingSpot(new ParkingSpot(1, null, false));
         ticket.setVehicleRegNumber(VEHICLE_REG_NUMBER);
         ticket.setPrice(15.0);
         ticket.setInTime(new Timestamp(System.currentTimeMillis()));
 
+        // Simule que 0 ligne à été affectée par la requête
         when(preparedStatement.executeUpdate()).thenReturn(0);
 
+        // Appel de la méthode saveTicket
         boolean result = ticketDAO.saveTicket(ticket);
 
+        // Vérifie que la méthode renvoie false
         assertFalse(result);
         verify(dataBaseConfig, times(1)).getConnection();
         verify(connection, times(1)).prepareStatement(DBConstants.SAVE_TICKET);
         verify(preparedStatement, times(1)).executeUpdate();
-        logger.info("Test SaveTicket_RowsAffectedEqualsZero Passed");
 
     }
 
+    // Test de la méthode getTicket pour un ticket valide
     @Test
-    void testGetTicket_Success() throws SQLException, ClassNotFoundException {
-        // Simulation de la récupération d'un ticket valide
+    void testGetTicket_Success() throws Exception, SQLException {
+        // Simulation de la récupération d'un ticket valide avec des valeurs spécifiques
         when(preparedStatement.executeQuery()).thenReturn(resultSet);
         when(resultSet.next()).thenReturn(true);
         when(resultSet.getInt("PARKING_NUMBER")).thenReturn(1);
@@ -173,8 +197,10 @@ class TicketDAOTest {
         when(resultSet.getTimestamp("IN_TIME")).thenReturn(new Timestamp(System.currentTimeMillis()));
         when(resultSet.getTimestamp("OUT_TIME")).thenReturn(null);
 
+        // Vérifie que le ticket récupéré est validé
         ticket = ticketDAO.getTicket(VEHICLE_REG_NUMBER);
 
+        // Vérifie que le ticket n'est pas nul
         assertNotNull(ticket);
         assertEquals(VEHICLE_REG_NUMBER, ticket.getVehicleRegNumber());
         assertEquals(1, ticket.getParkingSpot().getId());
@@ -182,38 +208,45 @@ class TicketDAOTest {
         assertNull(ticket.getOutTime());
     }
 
+    // Test de la méthode getTicket pour un ticket non trouvé
     @Test
-    void testGetTicket_NotFound() throws SQLException, ClassNotFoundException {
+    void testGetTicket_NotFound() throws Exception, SQLException {
         // Simulation d'un cas où aucun ticket n'est pas trouvé
         when(preparedStatement.executeQuery()).thenReturn(resultSet);
         when(resultSet.next()).thenReturn(false);
 
+        // Vérifie que le ticket récupéré est validé
         ticket = ticketDAO.getTicket(VEHICLE_REG_NUMBER);
+        // Vérifie que le ticket récupéré est validé mais il est nul
         assertNull(ticket);
     }
 
+    // Test de la méthode getTicket pour un ticket trouvé mais il y a une erreur
     @Test
-    void testGetTicket_Exception() throws SQLException, ClassNotFoundException {
-        // Simulation d'une erreur lors de la récupération du ticket
-        when(preparedStatement.executeQuery()).thenThrow(new SQLException("Erreur de base de données"));
+    void testGetTicket_Exception() throws Exception, SQLException {
+        // Simulation d'une erreur de base de donnée lors de la récupération du ticket
+        when(preparedStatement.executeQuery()).thenThrow(new SQLException("Database error"));
 
+        // Vérifie que le ticket récupéré est validé
         ticket = ticketDAO.getTicket(VEHICLE_REG_NUMBER);
-
+        // Vérifie que le ticket récupéré est validé mais il est nul
         assertNull(ticket);
-
     }
 
+    // Test de la méthode getNbTicket pour un ticket valide
     @Test
     void testGetNbTicket_Success() throws Exception {
-
+        // Simulation de la requête réussi avec 5 tickets
         when(preparedStatement.executeQuery()).thenReturn(resultSet);
         when(resultSet.next()).thenReturn(true);
         when(resultSet.getInt(1)).thenReturn(5);
 
+        // Appel de la méthode getNbTicket
         int ticketCount = ticketDAO.getNbTicket(VEHICLE_REG_NUMBER);
 
+        // Vérifie que le nombre de tiket est correct
         assertEquals(5, ticketCount);
-
+        // Vérifie que les méthodes ont été appelée une fois
         verify(dataBaseConfig, times(1)).getConnection();
         verify(connection, times(1)).prepareStatement(DBConstants.COUNT_TICKET);
         verify(preparedStatement, times(1)).setString(1, VEHICLE_REG_NUMBER);
@@ -224,13 +257,16 @@ class TicketDAOTest {
 
     @Test
     void testGetNbTicket_NoResult() throws Exception {
+        // Simulation de la requête réussi mais sans résultat
         when(preparedStatement.executeQuery()).thenReturn(resultSet);
         when(resultSet.next()).thenReturn(false);
 
+        // Appel de la méthode getNbTicket
         int ticketCount = ticketDAO.getNbTicket(VEHICLE_REG_NUMBER);
 
+        // Vérifie que le nombre de tiket est 0
         assertEquals(0, ticketCount);
-
+        // Vérifie que les méthodes ont été appelée une fois
         verify(dataBaseConfig, times(1)).getConnection();
         verify(connection, times(1)).prepareStatement(DBConstants.COUNT_TICKET);
         verify(preparedStatement, times(1)).setString(1, VEHICLE_REG_NUMBER);
@@ -240,72 +276,18 @@ class TicketDAOTest {
 
     @Test
     void testGetNbTicket_Exception() throws Exception {
-
+        // Simule une exception lors de l'éxécution de la requête
         when(preparedStatement.executeQuery()).thenThrow(new SQLException("Database error"));
 
+        // Appel de la méthode getNbTicket
         int ticketCount = ticketDAO.getNbTicket(VEHICLE_REG_NUMBER);
 
+        // Vérifie que le nombre de tiket est 0 en cas d'erreur
         assertEquals(0, ticketCount);
-
+        // Vérifie que les méthodes ont été appelée une fois
         verify(dataBaseConfig, times(1)).getConnection();
         verify(connection, times(1)).prepareStatement(DBConstants.COUNT_TICKET);
         verify(preparedStatement, times(1)).setString(1, VEHICLE_REG_NUMBER);
         verify(preparedStatement, times(1)).executeQuery();
-    }
-
-    @Test
-    void testUpdateTicket_Success() throws Exception {
-        // Simuler un comportement où la requête UPDATE réussit
-        when(preparedStatement.executeUpdate()).thenReturn(1); // Retourne 1 pour indiquer que 1 ligne a été mise à jour
-
-        Timestamp now = new Timestamp(System.currentTimeMillis());
-        when(ticket.getPrice()).thenReturn(10.0);
-        when(ticket.getOutTime()).thenReturn(now);
-        when(ticket.getId()).thenReturn(1);
-
-        boolean result = ticketDAO.updateTicket(ticket);
-
-        assertTrue(result);
-        verify(preparedStatement).setDouble(1, 10.0);
-
-        ArgumentCaptor<Timestamp> timestampCaptor = ArgumentCaptor.forClass(Timestamp.class);
-        verify(preparedStatement).setTimestamp(eq(2), timestampCaptor.capture());
-
-        Timestamp capturedTimestamp = timestampCaptor.getValue();
-
-        long tolerance = 1000;
-        assertTrue(Math.abs(capturedTimestamp.getTime() - now.getTime()) <= tolerance,
-                "La différence entre les timestamps est trop grande");
-
-        verify(preparedStatement).setInt(3, 1);
-        verify(preparedStatement).executeUpdate();
-    }
-
-    @Test
-    void testUpdateTicket_Failure() throws Exception {
-        // Simuler un comportement où la requête UPDATE échoue
-        when(preparedStatement.executeUpdate()).thenThrow(new SQLException("Error updating ticket"));
-
-        Timestamp now = new Timestamp(System.currentTimeMillis());
-        when(ticket.getPrice()).thenReturn(10.0);
-        when(ticket.getOutTime()).thenReturn(now);
-        when(ticket.getId()).thenReturn(1);
-
-        boolean result = ticketDAO.updateTicket(ticket);
-
-        assertFalse(result);
-        verify(preparedStatement).setDouble(1, 10.0);
-
-        ArgumentCaptor<Timestamp> timestampCaptor = ArgumentCaptor.forClass(Timestamp.class);
-        verify(preparedStatement).setTimestamp(eq(2), timestampCaptor.capture());
-
-        Timestamp capturedTimestamp = timestampCaptor.getValue();
-
-        long tolerance = 1000;
-        assertTrue(Math.abs(capturedTimestamp.getTime() - now.getTime()) <= tolerance,
-                "La différence entre les timestamps est trop grande");
-
-        verify(preparedStatement).setInt(3, 1);
-        verify(preparedStatement).executeUpdate();
     }
 }
